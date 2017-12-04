eac8ed6b8cea4507227dde2e7d33a3a837096e08
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-12-04 08:39:10 -0800

    dx(quality): Add the scope for the cli tool.

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,6 +11,7 @@ module.exports = {
                 // Relates to the whole package
                 'kikd',
                 // Parts of KI/KD
+                'cli',
                 // Developer Experience/Docs Scopes
                 'roadmap',
                 'worklog',
