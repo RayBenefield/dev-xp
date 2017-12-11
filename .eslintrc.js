8ffaea2dc51cac513ea36b4f4d871b9c9c77385d
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-12-11 06:13:39 -0800

    dx(quality): Create symlinks to make vim and eslint happy.

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+dx/eslint.config.js
--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    printWidth: 80,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    arrowParens: 'avoid',
-};
+./dx/prettier.config.js
