e7798ff686afa5d2e871870fbec00b5f3c069519
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-12-12 22:40:34 -0800

    dx(build): Change build process to use rollup instead of babel.

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import { minify } from 'uglify-es';
+import uglify from 'rollup-plugin-uglify';
+import filesize from 'rollup-plugin-filesize';
+import executable from 'rollup-plugin-executable';
+import pkg from './package.json';
+
+const babelConfig = {
+    babelrc: false,
+    presets: [['env', { modules: false, targets: { node: '6' } }], 'stage-0'],
+    plugins: [
+        [
+            'wildcard',
+            {
+                exts: ['js', 'es6', 'es', 'jsx', 'javascript', 'json'],
+                noCamelCase: true,
+            },
+        ],
+        'external-helpers',
+    ],
+    exclude: ['node_modules/**'],
+};
+
+export default [
+    {
+        input: 'src/node_modules/kikd/cli.js',
+        external: [...Object.keys(pkg.dependencies), 'fs', 'path'],
+        output: [
+            {
+                file: path.resolve('dist', Object.values(pkg.bin)[0]),
+                format: 'cjs',
+            },
+        ],
+        banner: '#!/usr/bin/env node',
+        plugins: [
+            resolve({
+                customResolveOptions: {
+                    moduleDirectory: 'src/node_modules',
+                },
+                preferBuiltins: true,
+            }),
+            babel(babelConfig),
+            uglify({}, minify),
+            executable(),
+            filesize(),
+        ],
+    },
+];
