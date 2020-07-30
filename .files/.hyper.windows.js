dd9fcdc5efdb8d1699f70a72e86b5da1ba4696bc
Raymond Benefield <raymond.benefield@gmail.com>
2020-07-29 21:53:59 -0700

    dx(hyper): Update windows main font.

--- .files/.hyper.windows.js
+++ .files/.hyper.windows.js
@@ -13,7 +13,7 @@ module.exports = {
 
         // font family with optional fallbacks
         fontFamily:
-            '"SauceCodePro Nerd Font", "Source Code Pro", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+            '"SauceCodePro NF", "Source Code Pro", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
         // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
         cursorColor: '#ccc',
