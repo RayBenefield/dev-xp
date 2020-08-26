6dfd6c284856f35e7efe80de58bdf2155ac30eee
Raymond Benefield <raymond.benefield@gmail.com>
2020-08-26 15:16:58 -0700

    dx(autohotkey): Add initial scripts and batch script to shortcut.

---
+++ .files/autohotkey/shortcut-to-startup.bat
@@ -0,0 +1,4 @@
+@echo off
+mklink "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\snap-windows.ahk" "C:\Users\%username%\Development\dev-xp\.files\autohotkey\snap-windows.ahk"
+mklink "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\remap-keys.ahk" "C:\Users\%username%\Development\dev-xp\.files\autohotkey\remap-keys.ahk"
+mklink "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\favorite-apps.ahk" "C:\Users\%username%\Development\dev-xp\.files\autohotkey\favorite-apps.ahk"
