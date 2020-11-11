4d3606dd2f17afdf5fce6d8b012d5afce6d840d8
Raymond Benefield <raymond.benefield@gmail.com>
2020-11-11 06:56:56 -0800

    dx(fish/paths): Add relative node_module bin paths.

--- .files/fish-plugins/paths/conf.d/paths.fish
+++ .files/fish-plugins/paths/conf.d/paths.fish
@@ -1,1 +1,1 @@
-set fish_user_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $fish_user_paths
+set fish_user_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin ./node_modules/.bin ../../../node_modules/.bin $fish_user_paths 
