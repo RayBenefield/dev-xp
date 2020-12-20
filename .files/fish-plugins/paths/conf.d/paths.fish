cafe2ee3f615d41aa4dc3d8d5de012aef7274679
Raymond Benefield <raymond.benefield@gmail.com>
2020-12-20 11:49:49 -0800

    dx(fish/paths): Add env variables for kak instead of vim.
    Closes #952

--- .files/fish-plugins/paths/conf.d/paths.fish
+++ .files/fish-plugins/paths/conf.d/paths.fish
@@ -1,1 +1,3 @@
-set fish_user_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin ./node_modules/.bin ../../../node_modules/.bin $fish_user_paths 
+set fish_user_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin ./node_modules/.bin ../../../node_modules/.bin $fish_user_paths
+set EDITOR kak
+set VISUAL kak
