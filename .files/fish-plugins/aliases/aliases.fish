bf784c0a2596749a2e37200b06d026bb81953f97
Raymond Benefield <raymond.benefield@gmail.com>
2020-12-31 20:08:46 -0800

    dx(fish/aliases): Conditionally add aliases based on os.
    Closes #1022

--- .files/fish-plugins/aliases/aliases.fish
+++ .files/fish-plugins/aliases/aliases.fish
@@ -5,5 +5,14 @@ alias yt='cd ~/Development/youtube'
 alias core='cd "/mnt/c/Users/RayBenefield/Dropbox/My PC (Ray-PC)/Documents/My Games/CORE/Saved/Maps"'
 alias xpf='wherein src/node_modules/'
 alias log='./dx/log'
-alias vim='kak'
-alias vi='/usr/bin/vim'
+
+set OS (uname)
+if test "$OS" = 'Linux'
+    alias vim='kak'
+    alias vi='/usr/bin/vim'
+end
+
+if test "$OS" = "MINGW64_NT-10.0-19041"
+    alias vim='/usr/bin/vim'
+    alias vi='/usr/bin/vim'
+end
