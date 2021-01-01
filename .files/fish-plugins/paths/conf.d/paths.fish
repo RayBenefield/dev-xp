5ff30c91e11fb30f2334840c1e1a35023e0e57a9
Raymond Benefield <raymond.benefield@gmail.com>
2020-12-31 16:34:42 -0800

    dx(fish/paths): Add cargo to path for future rust.

--- .files/fish-plugins/paths/conf.d/paths.fish
+++ .files/fish-plugins/paths/conf.d/paths.fish
@@ -1,3 +1,3 @@
-set fish_user_paths $HOME/.local/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin ./node_modules/.bin ../../../node_modules/.bin $fish_user_paths
+set fish_user_paths $HOME/.cargo/bin $HOME/.local/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin ./node_modules/.bin ../../../node_modules/.bin $fish_user_paths
 set EDITOR kak
 set VISUAL kak
